3d736ab7055d04a950a518c04b12efed3972eaf5
Bello Emmanuel Oluwatimilehin <Timilehin-bello@users.noreply.github.com>
2020-07-31 13:28:42 +0100

    Updates

---
+++ precache-manifest.91879bc624ba57536d064f2c0dc60abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1e4e0226660334074562865aa77955",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9715f9e703ee12aee541",
+    "url": "/monsters-rolodex/static/css/main.c779c610.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9715f9e703ee12aee541",
+    "url": "/monsters-rolodex/static/js/main.effbe774.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
